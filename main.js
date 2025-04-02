0c84ea2a614b61fdc4669f65f867ebabb302e29d
leandrobvs <112587531+leandrobvs@users.noreply.github.com>
2025-04-02 09:15:21 -0300

    final version

--- main.js
+++ main.js
@@ -1,24 +1,28 @@
 const langSwitcher = document.querySelector('.lang-switcher');
 
-langSwitcher.addEventListener('click', () => {
-  const currentLang = document.documentElement.getAttribute('lang');
+if (langSwitcher) {
+  langSwitcher.addEventListener('click', () => {
+    const currentLang = document.documentElement.getAttribute('lang');
 
-  if (currentLang === 'pt-br') {
-    document.documentElement.setAttribute('lang', 'en');
+    if (currentLang === 'pt-br') {
+      document.documentElement.setAttribute('lang', 'en');
 
-    document
-      .querySelectorAll('[lang="pt-br"]')
-      .forEach(element => (element.style.display = 'none'));
-    document.querySelectorAll('[lang="en"]').forEach(element => (element.style.display = 'block'));
-  } else {
-    document.documentElement.setAttribute('lang', 'pt-br');
+      document
+        .querySelectorAll('[lang="pt-br"]')
+        .forEach(element => (element.style.display = 'none'));
+      document
+        .querySelectorAll('[lang="en"]')
+        .forEach(element => (element.style.display = 'block'));
+    } else {
+      document.documentElement.setAttribute('lang', 'pt-br');
 
-    document.querySelectorAll('[lang="en"]').forEach(element => (element.style.display = 'none'));
-    document
-      .querySelectorAll('[lang="pt-br"]')
-      .forEach(element => (element.style.display = 'block'));
-  }
-});
+      document.querySelectorAll('[lang="en"]').forEach(element => (element.style.display = 'none'));
+      document
+        .querySelectorAll('[lang="pt-br"]')
+        .forEach(element => (element.style.display = 'block'));
+    }
+  });
+}
 
 document.addEventListener('DOMContentLoaded', function () {
   const floatingNavBtn = document.getElementById('floating-nav-btn');
