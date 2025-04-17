49f4fae81b316698aead17753f99849e93b532d9
leandrobvs <112587531+leandrobvs@users.noreply.github.com>
2025-04-17 11:17:45 -0300

    gtag-v1.1

--- main.js
+++ main.js
@@ -54,3 +54,45 @@ document.addEventListener('DOMContentLoaded', function () {
     observer.observe(sumarioSection);
   }
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+  const linksParaRastrear = [
+    'link-sobre-mim-pt',
+    'link-about-me-en',
+    'link-portfolio-pt',
+    'link-portfolio-en',
+    'link-curriculum-pt',
+    'link-curriculum-en',
+    'projeto-m1-marketing',
+    'projeto-etl',
+    'projeto-analise-financeira',
+    'projeto-analise-sentimento',
+    'projeto-analise-marketing',
+    'projeto-analise-rh',
+    'projeto-evasao-clientes',
+    'projeto-e-commerce',
+    'projeto-google-apps',
+    'projeto-excel-marketing',
+    'projeto-excel-demanda',
+    'projeto-excel-vpn',
+  ];
+
+  linksParaRastrear.forEach(id => {
+    const elemento = document.getElementById(id);
+    if (elemento) {
+      elemento.addEventListener('click', function () {
+        let eventCategory = 'geral';
+        if (id.startsWith('link-')) {
+          eventCategory = 'navegacao';
+        } else if (id.startsWith('projeto-')) {
+          eventCategory = 'portfolio';
+        }
+
+        gtag('event', 'click_link', {
+          event_category: eventCategory,
+          event_label: id,
+        });
+      });
+    }
+  });
+});
